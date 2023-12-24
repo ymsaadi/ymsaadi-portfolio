4deb16831c40237fdcfdaaaddb6f2f7bd494d9c6
ymsaadi <youssefmsaadi95@gmail.com>
2023-12-24 12:34:21 -0500

    Added and styled the background video.

--- src/App.js
+++ src/App.js
@@ -5,11 +5,13 @@ import Skills from "./skills/Skills";
 import Projects from "./projects/Projects";
 import Contact from "./contact/Contact";
 import "./styles/app.css";
+import Background from "./background/Background.js";
 
 const App = () => {
   return (
     <Router>
       <Nav />
+      <Background />
       <Routes>
         <Route path="/" element={<About />} />
         <Route path="/skills" element={<Skills />} />
