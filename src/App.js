d87a362ea9411f00f43ecb443996f9d5964cfe89
ymsaadi <youssefmsaadi95@gmail.com>
2023-12-25 10:39:14 -0500

    Added player stats (Full name, Age and Job title) component with styles.

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import Projects from "./projects/Projects";
 import Contact from "./contact/Contact";
 import "./styles/app.css";
 import Background from "./background/Background.js";
+import PlayerStats from "./playerStats/playerStats.js";
 
 const App = () => {
   return (
@@ -18,6 +19,7 @@ const App = () => {
         <Route path="/projects" element={<Projects />} />
         <Route path="/contact" element={<Contact />} />
       </Routes>
+      <PlayerStats />
     </Router>
   );
 };
