0d5cad85b581d53e11d9abe2746b27fc5750d353
ymsaadi <youssefmsaadi95@gmail.com>
2023-12-25 19:26:31 -0500

    Set up the About page data.

---
+++ src/about/subheadingsData.js
@@ -0,0 +1,75 @@
+const subheadingsData = {
+  1: [
+    {
+      title: "About me",
+      content: (
+        <>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit sit
+            repudiandae praesentium iste quaerat ratione doloribus velit odio
+            ipsum, numquam delectus deserunt accusantium accusamus, recusandae
+            totam in, harum magni similique.
+          </p>
+        </>
+      ),
+    },
+  ],
+  2: [
+    {
+      title: "2020 Masters Degree",
+      content: (
+        <>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit sit
+            repudiandae praesentium iste quaerat ratione doloribus velit odio
+            ipsum, numquam delectus deserunt accusantium accusamus, recusandae
+            totam in, harum magni similique.
+          </p>
+        </>
+      ),
+    },
+    {
+        title: "2017 Bachelors Degree",
+        content: (
+          <>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit sit
+              repudiandae praesentium iste quaerat ratione doloribus velit odio
+              ipsum, numquam delectus deserunt accusantium accusamus, recusandae
+              totam in, harum magni similique.
+            </p>
+          </>
+        ),
+      },
+  ],
+  3: [
+    {
+      title: "2021 - 2023 Software Developer",
+      content: (
+        <>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit sit
+            repudiandae praesentium iste quaerat ratione doloribus velit odio
+            ipsum, numquam delectus deserunt accusantium accusamus, recusandae
+            totam in, harum magni similique.
+          </p>
+        </>
+      ),
+    },
+    {
+        title: "2020 Software Developer Intern",
+        content: (
+          <>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit sit
+              repudiandae praesentium iste quaerat ratione doloribus velit odio
+              ipsum, numquam delectus deserunt accusantium accusamus, recusandae
+              totam in, harum magni similique.
+            </p>
+          </>
+        ),
+      },
+  ],
+};
+
+export default subheadingsData;
